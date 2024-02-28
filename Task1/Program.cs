567b7ddeee660b6a24eb378d993dbb19eaf5eb19
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-29 02:43:29 +0300

    Complete Task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//                               a x b
+// Вычислите значение формулы   ------- , где
+//                               c + d
+// a, b, c, d - целые числа. Решение в виде функции.
+
+using System.Runtime.InteropServices;
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    double denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+
+System.Console.WriteLine((CalculateFormula(1, 2, 2, 1)));
