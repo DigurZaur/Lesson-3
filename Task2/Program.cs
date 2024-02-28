f4d911842ae2788a5a1a2d0aec254b0c1276f46e
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-29 02:51:43 +0300

    Complete Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Функция для вывода квадратов чисел от 1 до N
+
+void square(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i*i} ");
+        i++;
+    }
+}
+square(5);
+System.Console.WriteLine();
+square(10);
+System.Console.WriteLine();
+square(15);
